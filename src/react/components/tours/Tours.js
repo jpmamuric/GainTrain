8f4136358ac26e6f2d0b5e108ef08cdb23f45f94
Jan Peter Mamuric <webdev.jmamuric@gmail.com>
2018-04-12 13:14:07 -0700

    added tours section

--- src/react/components/tours/Tours.js
+++ src/react/components/tours/Tours.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './Tours.css';
 
+import Data from './ToursData.json'
+
 const Tours = () => (
   <section className='section-tours'>
     <div className='util-center-text util-margin-bottom-big' >
@@ -9,18 +11,46 @@ const Tours = () => (
     </div>
 
     <div className='row'>
-      <div className='col-1-of-3'>
-        <div className='card'>
-          Card
-        </div>
-      </div>
-      <div className='col-1-of-3'>
-        Col 1 of 3
-      </div>
-      <div className='col-1-of-3'>
-        Col 1 of 3
-      </div>
+      {
+        Data.map(course => (
+          <div className='col-1-of-3'>
+            <div className='card'>
+              <div className='card__side card__side--front'>
+                <div className={`card__picture ${course.pictureModifier}`}/>
+                <h4 className='card__heading'>
+                  <span className={`card__heading-span ${course.headingModifier}`}>
+                    {course.heading}
+                  </span>
+                </h4>
+                <div className='card__details'>
+                  <ul>
+                    <li>{course.details.days}</li>
+                    <li>{course.details.size}</li>
+                    <li>Online: {course.details.online}</li>
+                    <li>Difficulty : {course.details.difficulty}</li>
+                  </ul>
+                </div>
+              </div>
+              <div className={`card__side card__side--back ${course.backModifier}`}>
+                <div className='card__cta'>
+                  <div className='card__price-box'>
+                    <p className='card__price-only'>Only</p>
+                    <p className='card__price-value'>${course.ctaPrice}</p>
+                  </div>
+                  <a href='/' className='btn btn--white'>Enroll Now!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      }
     </div>
+
+    <div className='util-center-text util-margin-top-big' >
+      <a href='/' className='btn btn--green'>Learn More</a>
+    </div>
+
+
   </section>
 )
 
