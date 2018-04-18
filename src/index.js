5505701979bf797ac5105b8f587fdfd136050495
Jan Peter Mamuric <webdev.jmamuric@gmail.com>
2018-04-17 21:05:15 -0700

    removed favicon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 import './assets/sass/main.scss';
 import App from './react/components/app/App';
@@ -10,8 +10,7 @@ import registerServiceWorker from './registerServiceWorker';
 import reducers from './redux/reducers/index';
 
 const store = createStore(
-  reducers,
-  composeWithDevTools()
+  reducers
 );
 
 const app = (
